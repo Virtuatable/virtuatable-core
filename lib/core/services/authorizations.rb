9d7c4c4c8391268553165fba2dfc042195474711
Vincent Courtois <vincent.courtois@coddity.com>
2022-05-19 19:39:41 +0200

    Fix #75 add a service method to create authorization

--- lib/core/services/authorizations.rb
+++ lib/core/services/authorizations.rb
@@ -8,6 +8,26 @@ module Core
     class Authorizations < Core::Services::Base
       include Singleton
 
+      # Creates an authorization to access to the data of the user for the application. The user is
+      # identified by the UUID of its current connection session.
+      # [WARNING] this method should NOT be used outside of the authentication backend.
+      #
+      # @param session_id [String] the unique identifier of the current session of the user.
+      # @param client_id [String] the unique public identifier of the application.
+      # @return [Code::Decorators::Authorization] the created authorization
+      #
+      # @raise [Core::Helpers::Errors::BadRequest] if any of the parameters is not given
+      # @raise [Core::Helpers::Errors::NotFound] if the session or the application is not found
+      def create_from_session(session_id: nil, client_id: nil, **ignored)
+        session = Core.svc.sessions.get_by_id(session_id: session_id)
+        application = Core.svc.applications.get_by_id(client_id: client_id)
+        authorization = Core::Models::OAuth::Authorization.create(
+          account: session.account,
+          application: application
+        )
+        Core::Decorators::Authorization.new(authorization)
+      end
+
       # Gets the authorization code corresponding to the provided value if it is linked to the
       # application matching the provided credentials. Otherwise it raises errors.
       #
